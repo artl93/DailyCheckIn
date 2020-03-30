4eb2f799e7d08bd1d59775a58adab2e3877a4ac9
Ren Silva <resilv@microsoft.com>
2020-03-30 11:32:48 +1100

    updated your details screen

---
+++ frontend/constants/Questions.js
@@ -0,0 +1,20 @@
+
+export default {
+    YES_NO : {'Y':'Yes','N':'No',null:'n/a'},
+    GENDER : {'F':'Female','M':'Male','O':'Other'},
+
+    AGE_GROUP : ['Younger than 2 years old',
+                    '2-4 years', 
+                    '5-9', 
+                    '10-18', 
+                    '19-29', 
+                    '30-39', 
+                    '40-49', 
+                    '50-59', 
+                    '60-64', 
+                    '65-69', 
+                    '70-79', 
+                    '80+' ],
+
+    STARTER_AGE : '30-39' 
+};
--- frontend/screens/PersonScreen.js
+++ frontend/screens/PersonScreen.js
@@ -2,40 +2,32 @@ import * as React from 'react'
 import { Alert,Picker, Text, TouchableHighlight, View, Switch } from 'react-native'
 import styles from '../styles/styles'
 import Spinner from 'react-native-loading-spinner-overlay'
-
+import MultipleChoice from '../components/MultipleChoice'
 
 import { ScrollView,  } from 'react-native-gesture-handler'
 
-
 import {insert} from '../apis/dcpdb'
 
-import t from 'tcomb-form-native' // 0.6.9
-
-var customStylesheet = require('../styles/bootstrap')
-
-// override globally the default stylesheet
 
 
-const Form = t.form.Form
-
-
-const Person = t.struct({
-  PostCode: t.String,
-  country: t.String,
-  otherInformation: t.String,
-})
-
-
-var options = {
-  stylesheet: customStylesheet
-}
-
+import Questions from '../constants/Questions'
 
 class PersonScreen extends React.Component {
 
   state = {
     // user state (TODO - pull this into its own model)
-
+    location:null,
+    ageGroup:Questions.STARTER_AGE,
+    gender:null,
+    diabetes:null,
+    cancer:null,
+    pregnancy:null,
+    asthma:null,
+    heartKidneyOrLiver:null,
+    weakImune:null,
+    traveled:null,
+    contact:null,
+    fluVaccine:null,
 
    // form UI state
     spinner:false,
@@ -58,10 +50,21 @@ class PersonScreen extends React.Component {
 
     this.setState( {spinner:true})
 
-    const userData = 
+    let userData = 
       { 
-        // TODO: convert to make this non-static
-        email: 'blogs@blogs.com',
+        location:this.state.location,
+        ageGroup:this.state.ageGroup,
+        gender:this.state.location,
+        diabetes:this.state.diabetes,
+        cancer:this.state.cancer,
+        pregnancy:this.state.pregnancy,
+        asthma:this.state.asthma,
+        heartKidneyOrLiver:this.state.heartKidneyOrLiver,
+        weakImune:this.state.weakImune,
+        traveled:this.state.traveled,
+        contact:this.state.contact,
+        fluVaccine:this.state.fluVaccine,
+        email: 'blogs@blogs.com'
       }
 
     insert( userData ).then( (data) => {
@@ -113,12 +116,66 @@ class PersonScreen extends React.Component {
 
           <View style={styles.form}>
 
+            {/* <Text style={styles.questionText}>Where are you located?</Text>
+                // still working in this */}
 
-            <Form ref="form" 
-                  type={ Person } 
-                  style={styles.form} 
-                  options={options}
-            /> 
+            <Text style={styles.questionText}>What is your age?</Text>
+            <Picker selectedValue ={this.state.ageGroup} onValueChange = {(value) => this.setState({ageGroup:value})}>
+              {Questions.AGE_GROUP.map((key) =>
+                <Picker.Item label={key} value={key} />
+              )}
+            </Picker>
+
+            <Text style={styles.questionText}>What is your gender?</Text>
+            <MultipleChoice value={this.state.gender}
+                            answers = {Questions.GENDER}
+                            parentSetState={ (keySelected) => this.setState({gender:keySelected})}/>
+
+            <Text style={styles.questionText}>Have you traveled outside your home country in the last 14 days? </Text>
+            <MultipleChoice value={this.state.travelled}
+                            answers = {Questions.YES_NO}
+                            parentSetState={ (keySelected) => this.setState({travelled:keySelected})}/>
+
+            <Text style={styles.questionText}>To the best of your knowledge, have you been in close contact with anyone who has a confirmed case of COVID-19? </Text>
+            <MultipleChoice value={this.state.contact}
+                            answers = {Questions.YES_NO}          
+                            parentSetState={ (keySelected) => this.setState({contact:keySelected})}/>
+
+            <Text style={styles.questionText}>Have you received a flu vaccine in the last 6 months? </Text>
+            <MultipleChoice value={this.state.fluVaccine}
+                            answers = {Questions.YES_NO}
+                            parentSetState={ (keySelected) => this.setState({fluVaccine:keySelected})}/>
+
+            <Text style={styles.questionText}>Have you been tested for COVID-19?</Text>
+            <MultipleChoice value={this.state.tested}
+                            answers = {Questions.YES_NO}
+                            parentSetState={ (keySelected) => this.setState({tested:keySelected})}/>
+
+            <Text style={styles.questionText}>Do you have any of the following conditions?</Text>
+
+            <MultipleChoice  questionText='Diabetes' value={this.state.diabetes}
+                            answers = {Questions.YES_NO}
+                            parentSetState={ (keySelected) => this.setState({diabetes:keySelected})}/>
+
+            <MultipleChoice questionText='Cancer' value={this.state.cancer}
+                            answers = {Questions.YES_NO}
+                            parentSetState={ (keySelected) => this.setState({cancer:keySelected})}/>
+
+            <MultipleChoice questionText='Pregnancy' value={this.state.pregnancy}
+                            answers = {Questions.YES_NO}
+                            parentSetState={ (keySelected) => this.setState({pregnancy:keySelected})}/> 
+
+            <MultipleChoice questionText='Asthma or chronic lung disease ' value={this.state.asthma}
+                            answers = {Questions.YES_NO}
+                            parentSetState={ (keySelected) => this.setState({asthma:keySelected})}/>
+
+            <MultipleChoice questionText='Heart, kidney, or liver conditions ' value={this.state.heartKidneyOrLiver}
+                            answers = {Questions.YES_NO}
+                            parentSetState={ (keySelected) => this.setState({heartKidneyOrLiver:keySelected})}/>
+
+            <MultipleChoice questionText='Weakened immune system ' value={this.state.weakImune}
+                            answers = {Questions.YES_NO}
+                            parentSetState={ (keySelected) => this.setState({weakImune:keySelected})}/>
 
 
             <View style={styles.buttonGroup}>
@@ -126,7 +183,7 @@ class PersonScreen extends React.Component {
                 <Text style={styles.buttonText}>Clear</Text>
               </TouchableHighlight>
               <TouchableHighlight style={[styles.button,styles.selected,styles.sideBySideR]} onPress={this.submit} >
-                <Text style={[styles.buttonText,styles.selectedButton]}>Submit</Text>
+                <Text style={[styles.buttonText,styles.selectedButton]}>Save</Text>
               </TouchableHighlight>
             </View>
 
